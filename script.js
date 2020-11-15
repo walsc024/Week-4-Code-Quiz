14c190d2fd14ef8e736c8a8641e43a20caaa7e5e
christopher walsh <chriswalsh08@googlemail.com>
2020-11-15 20:36:05 +0000

    started again nothing worked

--- script.js
+++ script.js
@@ -1,51 +1,4 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName("choice-text"));
 
-var currentQuestion = {};
-var acceptingAnswers = true;
-var score = 0;
-var questionCounter = 0;
-var availableQuestions = [];
-
-var quizQuestions = [{
-    question: "Ask a question",
-    choice1: "1",
-    choice2: "2",
-    choice3: "3",
-    choice: "4",
-    answer: 2,
-}, {
-    question: "Ask a question",
-    choice1: "1",
-    choice2: "2",
-    choice3: "3",
-    choice: "4",
-    correctAnswer: 2,
-}, {
-  question: "Ask a question",
-  choice1: "1",
-    choice2: "2",
-    choice3: "3",
-    choice: "4",
-  correctAnswer: 2,
-},{
-  question: "Ask a question",
-  choice1: "1",
-    choice2: "2",
-    choice3: "3",
-    choice: "4",
-  correctAnswer: 2,
-},{
-  question: "Ask a question",
-  choice1: "1",
-    choice2: "2",
-    choice3: "3",
-    choice: "4",
-  correctAnswer: 2,
-}]
-
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3
 
 
 var questionsSection = document.getElementById("questionsSection");
@@ -58,62 +11,101 @@ function startQuiz() {
   var startScreen = document.getElementById("startScreen");
   startScreen.style.display = "none";
   questionsSection.style.display = "block"
-  questionCounter = 0;
-  score = 0;
-  availableQuestions = [...quizQuestions]
-  console.log(availableQuestions);
-  getNewQuestion();
-  
-};
 
-getNewQuestion = () => {
-if (availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
-  localStorage.setItem('mostRecentScore', score)
+  function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
 }
-
-
-  questionCounter++
-  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-  progressBarFull.style.width = `${(questionsCounter/MAX_QUESTIONS) * 100}%`
-
-
-
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-  currentQuestion = availableQuestions[questionIndex];
-  question.innerText = currentQuestion.question;
-
-  choices.forEach( choices => {
-    const number = choices.dataset ['number'];
-    choices.innerText = currentQuestion['choice' + number];
-  })
-
-  availableQuestions.splice(questionIndex, 1)
-
-  acceptingAnswers = true
-};
-
-choices.forEach(choice => {
-  choice.addEventListener('click', e =>{
-    if (!acceptingAnswers) return
-
-    acceptingAnswers = false
-    const selectedChoice = e.target
-    const selectedAnswer = selectedChoice.dataset['number']
-  })
-})
-startQuiz();
-
-function startTimer() {
-  var timer = document.getElementById(“timerDisplay”);
-  countDown = setInterval(function secondsLeft() {
-    timeRemaining--;
-    timer.textContent = “timer: ” + timeRemaining;
-    if (timeRemaining < 0) {
-      endQuiz();
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
     }
-  }, 1000);
+ 
+    this.questionIndex++;
 }
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+// create questions here
+var questions = [
+    new Question("Inside which HTML element do we put JavaScript", ["<js>", "<javascript>","<scripting>", "<script>"], "<script>"),
+    new Question("JavaScript is a __ -side programming language", ["Client", "Server", "Both", "None"], "Both"),
+    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
+    new Question("Which of the following will write the message “Hello DataFlair!” in an alert box?", ["alertBox(“Hello DataFlair!”);", "alert(Hello DataFlair!);", "msgAlert(“Hello DataFlair!”);", "alert(“Hello DataFlair!”);"], "alert(“Hello DataFlair!”);"),
+    new Question("Which JavaScript label catches all the values, except for the ones specified?", ["catch", "label", "try", "default"], "default")
+];
+ 
+// create quiz
+var quiz = new Quiz(questions);
+ 
+// display quiz
+populate();
 
+}
 
 
 startBttn.onclick = startQuiz;
+
