9d16de583f4d558d45445a0ba581b53254e0668a
christopher walsh <chriswalsh08@googlemail.com>
2020-11-15 15:10:48 +0000

    added div's and classes to html javascript quest

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+var quizQuestions = [{
+    question: "Ask a question",
+    questionChoices: ["1", "2", "3"],
+    correctAnswer: "2"
+}, {
+    question: "Ask a question",
+    questionChoices: ["1", "2", "3"],
+    correctAnswer: "2"
+}, {
+  question: "Ask a question",
+  questionChoices: ["1", "2", "3"],
+  correctAnswer: "2"
+},{
+  question: "Ask a question",
+  questionChoices: ["1", "2", "3"],
+  correctAnswer: "2"
+},{
+  question: "Ask a question",
+  questionChoices: ["1", "2", "3"],
+  correctAnswer: "2"
+}]
+
+
+
+
+var questionsSection = document.getElementById("questionsSection");
+questionsSection.style.display = "none";
+var startBttn = document.getElementById("startBttn");
+
+// function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10);
+//         seconds = parseInt(timer % 60, 10);
+
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.textContent = minutes + ":" + seconds;
+
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+// }
+
+// window.onload = function () {
+//     var oneMinute = 60 * 1,
+//         display = document.querySelector('#time');
+//     startTimer(fiveMinutes, display);
+//};
+
+function startQuiz() {
+  var startScreen = document.getElementById("startScreen");
+  startScreen.style.display = "none";
+  questionsSection.style.display = "block";
+
+
 }
 
-window.onload = function () {
-    var oneMinute = 60 * 1,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
+startBttn.onclick = startQuiz;
